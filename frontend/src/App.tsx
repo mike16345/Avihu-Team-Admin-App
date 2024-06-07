c327e905cfe010e0e9a31f727cf5b2cfbedd3f6b
mikeg16345 <michaelgani815@gmail.com>
2024-06-07 15:49:15 +0300

    added theme toggle

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
 import "./App.css";
-import { HelloWorld } from "./components/HelloWorld";
+import { ModeToggle } from "./components/theme/mode-toggle";
+import { ThemeProvider } from "./components/theme/theme-provider";
+import { ViewDietPlanPage } from "./pages/ViewDietPlanPage";
 
 function App() {
-
   return (
-    <div>
-      <HelloWorld/>
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="flex w-full h-full  items-center">
+        <div className="w-1/4 h-full bg-secondary border-r-2 p-2">
+          <ModeToggle />
+        </div>
+        <div className="w-3/4 h-full p-8 ">
+          <ViewDietPlanPage />
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- frontend/src/components/HelloWorld.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export const HelloWorld = () => {
-  return <div>HelloWorld</div>;
-};
-
