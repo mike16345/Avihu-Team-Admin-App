e634cb75b6887ef83f64908d2b55c95ea9736b59
Noam Moskowitz <noammz101@gmail.com>
2024-06-27 14:55:35 +0300

    dynamically addd/remove excercises

--- frontend/src/components/workout plan/ExcerciseInput.tsx
+++ frontend/src/components/workout plan/ExcerciseInput.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ComboBox from './ComboBox'
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
@@ -10,6 +10,7 @@ import {
 import { ChevronsUpDown } from "lucide-react"
 import { Button } from '../ui/button'
 import { Textarea } from "@/components/ui/textarea"
+import { log } from 'console'
 
 
 interface ExcerciseInputProps {
@@ -20,36 +21,54 @@ interface ExcerciseInputProps {
 
 interface IExcercise {
     id: number,
-    name: string,
-    setToDo: number,
-    repsToDo: number,
+    name?: string,
+    setToDo?: number,
+    repsToDo?: number,
     tipFromTrainer?: string
 }
 
 
 const ExcerciseInput: React.FC<ExcerciseInputProps> = ({ options, handleSave, title }) => {
-    const [excercisesLength, setExcercisesLength] = useState(2)
-    const [workoutObjs, setWorkoutObjs] = useState<IExcercise[]>([])
+    const [workoutObjs, setWorkoutObjs] = useState<IExcercise[]>([
+        {
+            id: 1,
+            name: undefined,
+            setToDo: undefined,
+            repsToDo: undefined,
+            tipFromTrainer: undefined
+        }
+    ])
     const [s, setS] = useState()
 
     const handleChange = (e, index) => {
-        let name: string;
-        let value: string | number;
-        name = typeof (e) === `string` ? `name` : e.target.name;
-        value = typeof (e) === `string` ? e : e.target.value;
+        const name = typeof (e) === `string` ? `name` : e.target.name;
+        const value = typeof (e) === `string` ? e : e.target.value;
 
         setWorkoutObjs(prevWorkouts => {
-            const workoutExists = prevWorkouts.find(workout => workout.id === index);
-
-            if (workoutExists) {
-                return prevWorkouts.map(workout =>
-                    workout.id === index ? { ...workout, [name]: value } : workout
-                );
-            } else {
-                return [...prevWorkouts, { id: index, [name]: value }];
-            }
+            return prevWorkouts.map(workout =>
+                workout.id === index ? { ...workout, [name]: value } : workout
+            );
         });
+    };
+
+    const handleAddExcercise = () => {
+        const newObject = {
+            id: workoutObjs.length + 1,
+            name: undefined,
+            setToDo: undefined,
+            repsToDo: undefined,
+            tipFromTrainer: undefined
+        }
+
+        setWorkoutObjs([...workoutObjs, newObject]);
     }
+
+    const handleDeleteExcercise = (workoutId) => {
+        const newArr = workoutObjs.filter(workout => workout.id !== workoutId)
+
+        setWorkoutObjs(newArr)
+    }
+
     return (
         <div className='border-b-2 py-2 w-[70%]'>
             <Collapsible>
@@ -58,15 +77,15 @@ const ExcerciseInput: React.FC<ExcerciseInputProps> = ({ options, handleSave, ti
                     <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
                 </CollapsibleTrigger>
                 <CollapsibleContent>
-                    {Array.from({ length: excercisesLength }).map((item, index) => (
-                        <div className='py-5 flex items-center gap-5 border-b-4' key={index}>
+                    {workoutObjs.map(item => (
+                        <div className='py-5 flex items-center gap-5 border-b-4' key={item.id}>
                             <div className='flex flex-col gap-5'>
                                 <div className='flex gap-5 items-end'>
                                     <ComboBox
                                         options={options}
                                         setter={setS}
                                         handleChange={handleChange}
-                                        index={index}
+                                        index={item.id}
                                     />
 
                                     <div>
@@ -74,7 +93,8 @@ const ExcerciseInput: React.FC<ExcerciseInputProps> = ({ options, handleSave, ti
                                         <Input
                                             placeholder='1-10'
                                             name='setToDo'
-                                            onChange={(e) => handleChange(e, index)}
+                                            value={item.setToDo}
+                                            onChange={(e) => handleChange(e, item.id)}
                                         />
                                     </div>
                                     <div>
@@ -82,7 +102,8 @@ const ExcerciseInput: React.FC<ExcerciseInputProps> = ({ options, handleSave, ti
                                         <Input
                                             name='repsToDo'
                                             placeholder='8/10/12...'
-                                            onChange={(e) => handleChange(e, index)}
+                                            value={item.repsToDo}
+                                            onChange={(e) => handleChange(e, item.id)}
                                         />
                                     </div>
                                 </div>
@@ -91,17 +112,21 @@ const ExcerciseInput: React.FC<ExcerciseInputProps> = ({ options, handleSave, ti
                                     <Textarea
                                         placeholder='תלבש מכנסיים..'
                                         name='tipFromTrainer'
-                                        onChange={(e) => handleChange(e, index)}
+                                        value={item.tipFromTrainer}
+                                        onChange={(e) => handleChange(e, item.id)}
                                     />
                                 </div>
                             </div>
 
-                            <Button className='mr-5'>מחק</Button>
+                            <Button
+                                className='mr-5'
+                                onClick={() => handleDeleteExcercise(item.id)}
+                            >מחק</Button>
                         </div>
                     ))}
                     <Button
                         className='mt-2'
-                        onClick={() => setExcercisesLength(excercisesLength + 1)}
+                        onClick={handleAddExcercise}
                     >הוסף תרגיל</Button>
                     <Button
                         className='mr-2'
