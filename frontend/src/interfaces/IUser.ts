27232bb1becadb07a91b43abaedeeb8edd93d8d0
mikeg16345 <michaelgani815@gmail.com>
2024-07-14 17:44:01 +0300

    change graph color based off user plan type WIP

--- frontend/src/interfaces/IUser.ts
+++ frontend/src/interfaces/IUser.ts
@@ -1,7 +1,10 @@
+import { UserPlan } from "@/enums/User";
+
 export interface IUser {
   _id: string;
   name: string;
   email: string;
   password: string;
   createdAt: Date;
+  planType: UserPlan;
 }
