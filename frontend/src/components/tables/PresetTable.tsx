66d296002dd51ed576890fb4d9198ecf60089214
mikeg16345 <michaelgani815@gmail.com>
2024-07-23 20:33:26 +0300

    fix

--- frontend/src/components/tables/PresetTable.tsx
+++ frontend/src/components/tables/PresetTable.tsx
@@ -1,157 +1,145 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import {
-    Table,
-    TableBody,
-    TableCell,
-    TableFooter,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table"
-import TableActions from '../templates/workout templates/TableActions'
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import TableActions from "../templates/workout templates/TableActions";
 import {
-    Pagination,
-    PaginationLink,
-    PaginationNext,
-    PaginationPrevious,
-} from "@/components/ui/pagination"
+  Pagination,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from "@/components/ui/select"
-import { Input } from "@/components/ui/input"
-
-
-
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Input } from "@/components/ui/input";
 
 interface PresetTableProps {
-    data: any[];
-    handleDelete: (id: string) => void
-    navURL: string
+  data: any[];
+  handleDelete: (id: string) => void;
+  navURL: string;
 }
 
-
-
 const PresetTable: React.FC<PresetTableProps> = ({ data, handleDelete, navURL }) => {
-    const [displayData, setDisplayData] = useState<any[]>(data)
-    const [currentPage, setCurrentPage] = useState(1);
-    const [itemsPerPage, setItemsPerPage] = useState(5);
+  const [displayData, setDisplayData] = useState<any[]>(data);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(5);
 
-    const totalPages = Math.ceil(displayData.length / itemsPerPage);
+  const totalPages = Math.ceil(displayData.length / itemsPerPage);
 
-    const handlePageChange = (page: number) => {
-        setCurrentPage(page);
-    };
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
 
-    const handleNextPage = () => {
-        if ((currentPage) === totalPages) return
-        setCurrentPage(prevState => prevState + 1);
-    };
+  const handleNextPage = () => {
+    if (currentPage === totalPages) return;
+    setCurrentPage((prevState) => prevState + 1);
+  };
 
-    const handlePrevPage = () => {
-        if ((currentPage) === 1) return
-        setCurrentPage(prevState => prevState - 1);
-    };
+  const handlePrevPage = () => {
+    if (currentPage === 1) return;
+    setCurrentPage((prevState) => prevState - 1);
+  };
 
-    const handleItemsPerPageChange = (itemsPerPage: number) => {
-        setItemsPerPage(itemsPerPage);
-        setCurrentPage(1);
-    };
+  const handleItemsPerPageChange = (itemsPerPage: number) => {
+    setItemsPerPage(itemsPerPage);
+    setCurrentPage(1);
+  };
 
-    const paginatedData = displayData.slice(
-        (currentPage - 1) * itemsPerPage,
-        currentPage * itemsPerPage
-    );
+  const paginatedData = displayData.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
 
-    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { value } = e.target;
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
 
-        if (!value) {
-            setDisplayData(data)
-        }
-        const filteredArr = data.filter(item => item.itemName.includes(value))
-
-        setDisplayData(filteredArr)
+    if (!value) {
+      setDisplayData(data);
     }
+    const filteredArr = data.filter((item) => item.itemName.includes(value));
 
-    useEffect(() => {
-        setDisplayData(data)
-    }, [data])
+    setDisplayData(filteredArr);
+  };
 
-    return (
-        <div>
-            <div className='my-2 w-[300px]'>
-                <Input
-                    placeholder='חיפוש...'
-                    onChange={handleSearch}
-                />
-            </div>
-            <Table dir='rtl' className='w-auto min-w-[600px]'>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead className='text-right'>שם</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody >
-                    {paginatedData.length == 0 && (
-                        <TableRow className='font-bold text-center'>
-                            <TableCell>לא נמצאו תוצאות</TableCell>
-                        </TableRow>
-                    )}
-                    {paginatedData.map((data, i) => (
-                        <TableRow key={i}>
-                            <TableCell className='flex justify-between items-center px-3'>
-                                <div className='pr-4'>{data.itemName}</div>
-                                <div>
-                                    <TableActions
-                                        handleDelete={() => handleDelete(data._id)}
-                                        endPoint={`${navURL}/${data._id}`}
-                                    />
-                                </div>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-                <TableFooter className='p-2 flex items-start'>
-                    <Pagination>
-                        <PaginationPrevious
-                            onClick={handlePrevPage}
-                            className='cursor-pointer'
-                        />
-                        {Array.from({ length: totalPages }, (_, index) => (
-                            <PaginationLink
-                                key={index}
-                                onClick={() => handlePageChange(index + 1)}
-                                isActive={index + 1 === currentPage}
-                                className='cursor-pointer'
-                            >
-                                {index + 1}
-                            </PaginationLink>
-                        ))}
-                        <PaginationNext
-                            onClick={handleNextPage}
-                            className='cursor-pointer'
-                        />
-                    </Pagination>
-                    <Select onValueChange={(e) => handleItemsPerPageChange(Number(e))} value={itemsPerPage.toString()}>
-                        <SelectTrigger className='w-[80px]'>
-                            <SelectValue placeholder="10" />
-                        </SelectTrigger>
-                        <SelectContent>
-                            <SelectItem value="5">5</SelectItem>
-                            <SelectItem value="10">10</SelectItem>
-                            <SelectItem value="25">25</SelectItem>
-                        </SelectContent>
-                    </Select>
-                </TableFooter>
-            </Table>
-        </div>
-    );
+  useEffect(() => {
+    setDisplayData(data);
+  }, [data]);
+
+  return (
+    <div>
+      <div className="my-2 w-[300px]">
+        <Input placeholder="חיפוש..." onChange={handleSearch} />
+      </div>
+      <Table dir="rtl" className="w-auto min-w-[600px]">
+        <TableHeader>
+          <TableRow>
+            <TableHead className="text-right">שם</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {paginatedData.length == 0 && (
+            <TableRow className="font-bold text-center">
+              <TableCell>לא נמצאו תוצאות</TableCell>
+            </TableRow>
+          )}
+          {paginatedData.map((data, i) => (
+            <TableRow key={i}>
+              <TableCell className="flex justify-between items-center px-3">
+                <div className="pr-4">{data.presetName || data.itemName}</div>
+                <div>
+                  <TableActions
+                    handleDelete={() => handleDelete(data._id)}
+                    endPoint={`${navURL}/${data._id}`}
+                  />
+                </div>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter className="p-2 flex items-start">
+          <Pagination>
+            <PaginationPrevious onClick={handlePrevPage} className="cursor-pointer" />
+            {Array.from({ length: totalPages }, (_, index) => (
+              <PaginationLink
+                key={index}
+                onClick={() => handlePageChange(index + 1)}
+                isActive={index + 1 === currentPage}
+                className="cursor-pointer"
+              >
+                {index + 1}
+              </PaginationLink>
+            ))}
+            <PaginationNext onClick={handleNextPage} className="cursor-pointer" />
+          </Pagination>
+          <Select
+            onValueChange={(e) => handleItemsPerPageChange(Number(e))}
+            value={itemsPerPage.toString()}
+          >
+            <SelectTrigger className="w-[80px]">
+              <SelectValue placeholder="10" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="5">5</SelectItem>
+              <SelectItem value="10">10</SelectItem>
+              <SelectItem value="25">25</SelectItem>
+            </SelectContent>
+          </Select>
+        </TableFooter>
+      </Table>
+    </div>
+  );
 };
 
-
-export default PresetTable
+export default PresetTable;
