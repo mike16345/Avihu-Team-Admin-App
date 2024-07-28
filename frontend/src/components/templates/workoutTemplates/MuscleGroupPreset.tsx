be6d60f00a5f486364af94d4897d58425942ac16
mikeg16345 <michaelgani815@gmail.com>
2024-07-28 18:04:37 +0300

    added some spacing and validation for name of preset.

--- frontend/src/components/templates/workoutTemplates/MuscleGroupPreset.tsx
+++ frontend/src/components/templates/workoutTemplates/MuscleGroupPreset.tsx
@@ -1,128 +1,125 @@
-import { Collapsible, CollapsibleContent } from '@/components/ui/collapsible';
-import { Input } from '@/components/ui/input';
-import DeleteButton from '@/components/workout plan/buttons/DeleteButton';
-import DeleteModal from '@/components/workout plan/DeleteModal';
-import { IMuscleGroupWorkouts, IWorkout } from '@/interfaces/IWorkoutPlan';
-import React, { useState } from 'react'
-import { Button } from '@/components/ui/button';
-import { FaChevronDown } from 'react-icons/fa';
-import WorkoutContainerPreset from './WorkoutContainerPreset';
-
+import { Collapsible, CollapsibleContent } from "@/components/ui/collapsible";
+import { Input } from "@/components/ui/input";
+import DeleteButton from "@/components/workout plan/buttons/DeleteButton";
+import DeleteModal from "@/components/workout plan/DeleteModal";
+import { IMuscleGroupWorkouts, IWorkout } from "@/interfaces/IWorkoutPlan";
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { FaChevronDown } from "react-icons/fa";
+import WorkoutContainerPreset from "./WorkoutContainerPreset";
 
 interface MuscleGroupPresetProps {
-    handleSave: (workouts: IMuscleGroupWorkouts[]) => void;
-    title: string;
-    workout: IMuscleGroupWorkouts[];
-    handlePlanNameChange: (newName: string) => void;
-    handleDeleteWorkout: () => void
+  handleSave: (workouts: IMuscleGroupWorkouts[]) => void;
+  title: string;
+  workout: IMuscleGroupWorkouts[];
+  handlePlanNameChange: (newName: string) => void;
+  handleDeleteWorkout: () => void;
 }
 
 const MuscleGroupPreset: React.FC<MuscleGroupPresetProps> = ({
-    handleSave,
-    title,
-    workout,
-    handlePlanNameChange,
-    handleDeleteWorkout
+  handleSave,
+  title,
+  workout,
+  handlePlanNameChange,
+  handleDeleteWorkout,
 }) => {
+  const [planeName, setPlanName] = useState<string | undefined>();
+  const [workouts, setWorkouts] = useState<IMuscleGroupWorkouts[]>(workout);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
 
-    const [planeName, setPlanName] = useState<string | undefined>();
-    const [workouts, setWorkouts] = useState<IMuscleGroupWorkouts[]>(workout);
-    const [isOpen, setIsOpen] = useState(false);
-    const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
-
-    const addWorkout = () => {
-        const newObject: IMuscleGroupWorkouts = {
-            muscleGroup: ``,
-            exercises: [],
-        };
-
-        if (workouts[0] == undefined) {
-            setWorkouts([newObject]);
-            handleSave([newObject]);
-            return;
-        }
-        setWorkouts([...workouts, newObject]);
-        handleSave([...workouts, newObject]);
+  const addWorkout = () => {
+    const newObject: IMuscleGroupWorkouts = {
+      muscleGroup: ``,
+      exercises: [],
     };
 
-    const updateWorkouts = (i: number, workoutsObject: IWorkout[]) => {
-        const updatedWorkouts = [...workouts];
-        updatedWorkouts[i] = {
-            ...workouts[i],
-            exercises: workoutsObject,
-        };
+    if (workouts[0] == undefined) {
+      setWorkouts([newObject]);
+      handleSave([newObject]);
+      return;
+    }
+    setWorkouts([...workouts, newObject]);
+    handleSave([...workouts, newObject]);
+  };
 
-        setWorkouts(updatedWorkouts);
-        handleSave(updatedWorkouts);
+  const updateWorkouts = (i: number, workoutsObject: IWorkout[]) => {
+    const updatedWorkouts = [...workouts];
+    updatedWorkouts[i] = {
+      ...workouts[i],
+      exercises: workoutsObject,
     };
 
-    const updateMuscleGroup = (i: number, value: string) => {
-        const updatedWorkouts = [...workouts];
-        updatedWorkouts[i] = {
-            ...workouts[i],
-            muscleGroup: value,
-        };
+    setWorkouts(updatedWorkouts);
+    handleSave(updatedWorkouts);
+  };
 
-        setWorkouts(updatedWorkouts);
-        handleSave(updatedWorkouts);
+  const updateMuscleGroup = (i: number, value: string) => {
+    const updatedWorkouts = [...workouts];
+    updatedWorkouts[i] = {
+      ...workouts[i],
+      muscleGroup: value,
     };
 
-    const deleteMuscleGroup = (index: number) => {
-        const muscleGroupCopy = workouts.filter((_, i) => i !== index);
+    setWorkouts(updatedWorkouts);
+    handleSave(updatedWorkouts);
+  };
 
-        setWorkouts(muscleGroupCopy);
-        handleSave(muscleGroupCopy);
-    };
+  const deleteMuscleGroup = (index: number) => {
+    const muscleGroupCopy = workouts.filter((_, i) => i !== index);
 
+    setWorkouts(muscleGroupCopy);
+    handleSave(muscleGroupCopy);
+  };
 
-    return (
-        <>
-        <div className="border-b-2  last:border-b-0  rounded py-2 ">
-            <Collapsible defaultOpen={isOpen} open={isOpen} onOpenChange={setIsOpen}>
-                <div className="flex items-center justify-between gap-4 w-full font-bold text-lg">
-                    <Input
-                        className="w-64"
-                        onClick={(e) => e.stopPropagation()}
-                        onChange={(e) => setPlanName(e.target.value)}
-                        onBlur={planeName ? () => handlePlanNameChange(planeName) : () => { }}
-                        value={planeName ? planeName : planeName == `` ? planeName : title}
-                    />
-                    <div className='flex items-center gap-4'>
-                        <DeleteButton tip="הסר אימון" onClick={() => setIsDeleteModalOpen(true)} />
-                        <Button
-                                onClick={() => setIsOpen((state) => !state)}
-                                variant="ghost"
-                                size="sm"
-                                className={`w-9 p-0 transition ${isOpen ? "rotate-180" : "rotate-0"}`}
-                            >
-                                <FaChevronDown className="h-4 w-4" />
-                                <span className="sr-only">Toggle</span>
-                            </Button>
-                    </div>
-                </div>
-                <CollapsibleContent>
-                    {workouts.map((workout, i) => (
-                        <WorkoutContainerPreset
-                            key={i}
-                            muscleGroup={workout}
-                            handleUpdateWorkouts={(workouts) => updateWorkouts(i, workouts)}
-                            handleUpdateMuscleGroup={(value) => updateMuscleGroup(i, value)}
-                            handleDeleteMuscleGroup={() => deleteMuscleGroup(i)}
-                        />
+  return (
+    <>
+      <div className="border-b-2  last:border-b-0  rounded py-2 ">
+        <Collapsible defaultOpen={isOpen} open={isOpen} onOpenChange={setIsOpen}>
+          <div className="flex items-center justify-between gap-4 w-full font-bold text-lg">
+            <Input
+              className="w-64"
+              onClick={(e) => e.stopPropagation()}
+              onChange={(e) => setPlanName(e.target.value)}
+              onBlur={planeName ? () => handlePlanNameChange(planeName) : () => {}}
+              value={planeName ? planeName : planeName == `` ? planeName : title}
+            />
+            <div className="flex items-center gap-4">
+              <DeleteButton tip="הסר אימון" onClick={() => setIsDeleteModalOpen(true)} />
+              <Button
+                onClick={() => setIsOpen((state) => !state)}
+                variant="ghost"
+                size="sm"
+                className={`w-9 p-0 transition ${isOpen ? "rotate-180" : "rotate-0"}`}
+              >
+                <FaChevronDown className="h-4 w-4" />
+                <span className="sr-only">Toggle</span>
+              </Button>
+            </div>
+          </div>
+          <CollapsibleContent>
+            {workouts.map((workout, i) => (
+              <WorkoutContainerPreset
+                key={i}
+                muscleGroup={workout}
+                handleUpdateWorkouts={(workouts) => updateWorkouts(i, workouts)}
+                handleUpdateMuscleGroup={(value) => updateMuscleGroup(i, value)}
+                handleDeleteMuscleGroup={() => deleteMuscleGroup(i)}
+              />
             ))}
-                    <Button onClick={addWorkout} className="my-2">
-                        הוסף קבוצת שריר
-                    </Button>
-                </CollapsibleContent>
-            </Collapsible>
-        </div>
-        <DeleteModal
+            <Button onClick={addWorkout} className="my-2">
+              הוסף קבוצת שריר
+            </Button>
+          </CollapsibleContent>
+        </Collapsible>
+      </div>
+      <DeleteModal
         isModalOpen={isDeleteModalOpen}
         setIsModalOpen={setIsDeleteModalOpen}
         onConfirm={handleDeleteWorkout}
-        />
-        </>
-    )
-}
+      />
+    </>
+  );
+};
 
-export default MuscleGroupPreset
+export default MuscleGroupPreset;
--- frontend/src/components/templates/workoutTemplates/WorkoutContainerPreset.tsx
+++ frontend/src/components/templates/workoutTemplates/WorkoutContainerPreset.tsx
@@ -7,93 +7,72 @@ import { ChevronsUpDown } from "lucide-react";
 import { useState } from "react";
 import ExerciseInputpreset from "./ExerciseInputpreset";
 
-const muscleGroups: string[] = [
-    "חזה",
-    "כתפיים",
-    "יד אחורית",
-    "גב",
-    "יד קידמית",
-    "רגליים",
-    "בטן",
-    "אירובי",
-];
-
-const chestExercises: string[] = [
-    "פרפר",
-    "מקבילים",
-    "לחיצת חזה בשיפוע שלילי",
-    "לחיצת חזה בשיפוע חיובי",
-    "לחיצת חזה",
-];
-const shoulderExercises: string[] = ["כתפיים"];
-
 interface IWorkoutContainerPresetProps {
-    muscleGroup: IMuscleGroupWorkouts;
-    handleUpdateMuscleGroup: (value: string) => void;
-    handleUpdateWorkouts: (workoutsObject: IWorkout[]) => void;
-    handleDeleteMuscleGroup: () => void;
+  muscleGroup: IMuscleGroupWorkouts;
+  handleUpdateMuscleGroup: (value: string) => void;
+  handleUpdateWorkouts: (workoutsObject: IWorkout[]) => void;
+  handleDeleteMuscleGroup: () => void;
 }
 
 const WorkoutContainerPreset: React.FC<IWorkoutContainerPresetProps> = ({
-    muscleGroup,
-    handleUpdateMuscleGroup,
-    handleUpdateWorkouts,
-    handleDeleteMuscleGroup,
+  muscleGroup,
+  handleUpdateMuscleGroup,
+  handleUpdateWorkouts,
+  handleDeleteMuscleGroup,
 }) => {
+  const [isDeleteMuscleGroupModalOpen, setIsDeleteMuscleGroupModalOpen] = useState(false);
+  const [openMuscleGroupContainer, setOpenMuscleGroupContainer] = useState(false);
 
-    const [isDeleteMuscleGroupModalOpen, setIsDeleteMuscleGroupModalOpen] = useState(false);
-    const [openMuscleGroupContainer, setOpenMuscleGroupContainer] = useState(false);
+  return (
+    <Collapsible
+      open={openMuscleGroupContainer}
+      onOpenChange={setOpenMuscleGroupContainer}
+      className=" rounded px-3 py-4 "
+    >
+      <>
+        <div className="flex w-full items-center border-b-2 last:border-b-0 ">
+          <div className="flex  py-2 items-center w-full justify-between">
+            <div className="flex  items-center gap-2 ">
+              <h4 className=" font-bold">קבוצת שריר -</h4>
 
-    return (
-        <Collapsible
-            open={openMuscleGroupContainer}
-            onOpenChange={setOpenMuscleGroupContainer}
-            className=" rounded px-3 py-4 "
-        >
-            <>
-                <div className="flex w-full items-center border-b-2 last:border-b-0 ">
-                    <div className="flex  py-2 items-center w-full justify-between">
-                        <div className="flex  items-center gap-2 ">
-                            <h4 className=" font-bold">קבוצת שריר -</h4>
+              <MuscleGroupSelector
+                handleChange={(value) => handleUpdateMuscleGroup(value)}
+                existingMuscleGroup={muscleGroup.muscleGroup}
+              />
+            </div>
+            <div className="flex items-center justify-between gap-4">
+              <DeleteButton
+                tip="הסר קבוצת שריר"
+                onClick={() => setIsDeleteMuscleGroupModalOpen(true)}
+              />
+              <ChevronsUpDown
+                onClick={() => setOpenMuscleGroupContainer((open) => !open)}
+                className="ml-2 h-4 w-4 hover:cursor-pointer opacity-50"
+              />
+            </div>
+          </div>
+        </div>
+        <CollapsibleContent>
+          <>
+            <ExerciseInputpreset
+              options={muscleGroup.muscleGroup || undefined}
+              exercises={muscleGroup.exercises}
+              updateWorkouts={(workouts) => handleUpdateWorkouts(workouts)}
+            />
+          </>
+        </CollapsibleContent>
 
-                            <MuscleGroupSelector
-                                handleChange={(value) => handleUpdateMuscleGroup(value)}
-                                existingMuscleGroup={muscleGroup.muscleGroup}
-                            />
-                        </div>
-                        <div className="flex items-center justify-between gap-4">
-                            <DeleteButton
-                                tip="הסר קבוצת שריר"
-                                onClick={() => setIsDeleteMuscleGroupModalOpen(true)}
-                            />
-                            <ChevronsUpDown
-                                onClick={() => setOpenMuscleGroupContainer((open) => !open)}
-                                className="ml-2 h-4 w-4 hover:cursor-pointer opacity-50"
-                            />
-                        </div>
-                    </div>
-                </div>
-                <CollapsibleContent>
-                    <>
-                        <ExerciseInputpreset
-                            options={muscleGroup.muscleGroup || undefined}
-                            exercises={muscleGroup.exercises}
-                            updateWorkouts={(workouts) => handleUpdateWorkouts(workouts)}
-                        />
-                    </>
-                </CollapsibleContent>
+        <DeleteModal
+          isModalOpen={isDeleteMuscleGroupModalOpen}
+          setIsModalOpen={setIsDeleteMuscleGroupModalOpen}
+          onConfirm={() => {
+            setIsDeleteMuscleGroupModalOpen(false);
+            handleDeleteMuscleGroup();
+          }}
+        />
+      </>
+    </Collapsible>
+  );
+};
 
-                <DeleteModal
-                    isModalOpen={isDeleteMuscleGroupModalOpen}
-                    setIsModalOpen={setIsDeleteMuscleGroupModalOpen}
-                    onConfirm={() => {
-                        setIsDeleteMuscleGroupModalOpen(false);
-                        handleDeleteMuscleGroup();
-                    }}
-                />
-            </>
-        </Collapsible>
-    )
-}
-
-export default WorkoutContainerPreset
+export default WorkoutContainerPreset;
