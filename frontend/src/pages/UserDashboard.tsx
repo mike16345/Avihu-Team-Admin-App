7bc27444ea29fb59254782bf33a729e85a6fae88
Noam Moskowitz <noammz101@gmail.com>
2024-08-18 15:07:39 +0300

    added presenting name of user you are looking at in dashboard

--- frontend/src/pages/UserDashboard.tsx
+++ frontend/src/pages/UserDashboard.tsx
@@ -1,15 +1,30 @@
 import { WeightProgression } from "@/components/UserDashboard/WeightProgression/WeightProgression";
 import { WorkoutProgression } from "@/components/UserDashboard/WorkoutProgression/WorkoutProgression";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useUsersApi } from "@/hooks/useUsersApi";
+import { IUser } from "@/interfaces/IUser";
+import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 export const UserDashboard = () => {
   const { id } = useParams();
+  const {getUser}=useUsersApi()
+
+  const [user,setUser]=useState<IUser|null>(null)
+
+  useEffect(()=>{
+    if (!id) return 
+
+    getUser(id)
+    .then(res=>setUser(res))
+    .catch(err=>console.log(err))
+
+  },[])
 
   return (
     <div className="size-full flex flex-col gap-2">
       <div className="flex items-center justify-between">
-        <h1 className="text-2xl font-bold">אביהו בושרי</h1>
+        <h1 className="text-2xl font-bold">{user?`${user?.firstName} ${user?.lastName}`:``}</h1>
         <ul className="flex flex-col text-sm ">
           <Link
             className=" hover:bg-secondary font-bold px-2 py-0.5 rounded-md"
