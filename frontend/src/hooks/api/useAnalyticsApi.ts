0e85ab893e295faf8613746acc1bb863005de566
mike16345 <95699872+mike16345@users.noreply.github.com>
2024-09-05 23:11:02 +0300

    analytics api refactored

--- frontend/src/hooks/api/useAnalyticsApi.ts
+++ frontend/src/hooks/api/useAnalyticsApi.ts
@@ -1,14 +1,17 @@
 import { fetchData, patchItem } from "@/API/api";
 import { UsersCheckIn } from "@/interfaces/IAnalytics";
-import React from "react";
+import { ApiResponse } from "@/types/types";
 
-const ANALYTICS_ENDPOINT = `analytics/`;
+const ANALYTICS_ENDPOINT = `analytics`;
 
 const useAnalyticsApi = () => {
-  const getAllCheckInUsers = () => fetchData<UsersCheckIn[]>(ANALYTICS_ENDPOINT + `checkIns`);
+  const getAllCheckInUsers = () =>
+    fetchData<ApiResponse<UsersCheckIn[]>>(ANALYTICS_ENDPOINT + `/checkIns`).then(
+      (res) => res.data
+    );
 
   const checkOffUser = (id: string) =>
-    patchItem<UsersCheckIn>(ANALYTICS_ENDPOINT + `checkIns/` + id);
+    patchItem<UsersCheckIn>(ANALYTICS_ENDPOINT + `/checkIns/one?id=${id}`);
 
   return { getAllCheckInUsers, checkOffUser };
 };
