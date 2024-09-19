b2171727d681541d8a637520fe0df0c47094dddc
mike16345 <95699872+mike16345@users.noreply.github.com>
2024-09-19 03:33:50 +0300

    added ability to double click on row to view item

--- frontend/src/components/tables/DataTableHebrew.tsx
+++ frontend/src/components/tables/DataTableHebrew.tsx
@@ -179,7 +179,11 @@ export function DataTableHebrew<TData, TValue>({
           <TableBody>
             {table.getRowModel().rows?.length ? (
               table.getRowModel().rows.map((row) => (
-                <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
+                <TableRow
+                  key={row.id}
+                  onDoubleClick={(e) => handleViewData(row.original)}
+                  data-state={row.getIsSelected() && "selected"}
+                >
                   {row.getVisibleCells().map((cell) => (
                     <TableCell key={cell.id}>
                       {flexRender(cell.column.columnDef.cell, cell.getContext())}
