1336be43d32a3528e41e75eeb002c1da46b2ebf8
Noam Moskowitz <noammz101@gmail.com>
2024-12-11 13:14:49 +0200

    changed toast alerts in blogs to hebrew

--- frontend/src/components/Blog/BlogEditor.tsx
+++ frontend/src/components/Blog/BlogEditor.tsx
@@ -78,12 +78,12 @@ const BlogEditor = () => {
 
   const onSuccess = () => {
     query.invalidateQueries({ queryKey: [QueryKeys.BLOGS] });
-    toast.success(`${!isEdit ? "Created" : "Updated"} blog successfully!`);
+    toast.success(`פוסט נשמר בהצלחה!`);
     navigate("/blogs");
   };
 
   const onError = () => {
-    toast.error("Failed to save the blog. Please try again.");
+    toast.error("אירעה שגיאה בהעלאת הפוסט!");
   };
 
   const saveBlogMutation = useMutation({ mutationFn: handleSave, onSuccess, onError });
@@ -115,7 +115,7 @@ const BlogEditor = () => {
       <BackButton navLink="/blogs" />
       <Label className="font-semibold">כותרת</Label>
       <Input
-        className="w-1/2"
+        className="w-1/2 "
         value={blog.title}
         onChange={(e) => handleFieldChange("title", e.target.value)}
       />
