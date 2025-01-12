35b31d9e7dfa4e318fbd145e23d214625bb61d2a
mike16345 <95699872+mike16345@users.noreply.github.com>
2025-01-12 17:10:20 +0200

    import movdd

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
 import { Sidebar } from "./components/Navbar/Sidebar";
 import RequireAuth from "./hooks/Authentication/RequireAuthentication";
 import useAuth from "./hooks/Authentication/useAuth";
 import LoginPage from "./pages/LoginPage";
 import { AppRoutes } from "./routes/AppRoutes";
+import "./App.css";
 
 function App() {
   const { authed } = useAuth();
