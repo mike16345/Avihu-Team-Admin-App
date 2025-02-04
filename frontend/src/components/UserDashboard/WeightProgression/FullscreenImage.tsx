259af99ef5daca9dbf7e20d865b3d793f6da7757
mike16345 <95699872+mike16345@users.noreply.github.com>
2025-02-04 15:35:30 +0200

    implemented download user image

--- frontend/src/components/UserDashboard/WeightProgression/FullscreenImage.tsx
+++ frontend/src/components/UserDashboard/WeightProgression/FullscreenImage.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import { FaArrowLeft, FaArrowRight, FaDownload } from "react-icons/fa";
 import { IoClose } from "react-icons/io5";
 
 interface FullscreenImageProps {
@@ -17,12 +17,44 @@ export const FullscreenImage: FC<FullscreenImageProps> = ({
   onArrowPress,
   onClose,
 }) => {
+  const downloadFile = async (url: string, filename: string) => {
+    const response = await fetch(url, {
+      method: "GET",
+    });
+
+    const blob = await response.blob();
+
+    const blobUrl = window.URL.createObjectURL(blob);
+
+    const link = document.createElement("a");
+
+    link.href = blobUrl;
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+    window.URL.revokeObjectURL(blobUrl);
+  };
+
+  const handleDownload = () => {
+    const filename = `user-image.jpg`;
+    downloadFile(img, filename);
+  };
+
   return (
-    <div className="fixed top-0 left-0  size-full glassmorphism z-50 flex items-center justify-center">
-      <div className="absolute top-4 left-4">
+    <div className="fixed top-0 left-0 size-full glassmorphism z-50 flex items-center justify-center">
+      <div className="absolute top-4 left-4 flex gap-4">
         <IoClose
           onClick={onClose}
-          className=" text-white  filter-black cursor-pointer font-bold"
+          className="text-white filter-black cursor-pointer font-bold"
+          size={36}
+        />
+      </div>
+      <div className="absolute top-4 right-4 flex gap-4">
+        <FaDownload
+          onClick={handleDownload}
+          className="text-white filter-black cursor-pointer font-bold"
           size={36}
         />
       </div>
@@ -30,7 +62,7 @@ export const FullscreenImage: FC<FullscreenImageProps> = ({
         <div className="absolute top-1/2 left-4">
           <FaArrowLeft
             onClick={() => onArrowPress("previous")}
-            className=" text-white  filter-black cursor-pointer font-bold"
+            className="text-white filter-black cursor-pointer font-bold"
             size={36}
           />
         </div>
@@ -38,13 +70,13 @@ export const FullscreenImage: FC<FullscreenImageProps> = ({
       <img
         src={img}
         alt="fullscreen"
-        className="max-w-full max-h-full rounded-md py-2 shadow-black "
+        className="max-w-full max-h-full rounded-md py-2 shadow-black"
       />
       {onArrowPress && isNext && (
         <div className="absolute top-1/2 right-4">
           <FaArrowRight
             onClick={() => onArrowPress("next")}
-            className=" text-white  filter-black cursor-pointer font-bold"
+            className="text-white filter-black cursor-pointer font-bold"
             size={36}
           />
         </div>
