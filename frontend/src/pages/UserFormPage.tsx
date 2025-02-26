f996cd85c5941e5f47c4282d8bf2cd97105f0f72
mike16345 <95699872+mike16345@users.noreply.github.com>
2025-02-26 19:35:39 +0200

    implemented query and mutation hooks to user forms and table

--- frontend/src/pages/UserFormPage.tsx
+++ frontend/src/pages/UserFormPage.tsx
@@ -1,46 +1,20 @@
 import UserForm from "@/components/forms/UserForm";
 import BackButton from "@/components/ui/BackButton";
 import Loader from "@/components/ui/Loader";
-import { ERROR_MESSAGES } from "@/enums/ErrorMessages";
 import { MainRoutes } from "@/enums/Routes";
-import { useUsersApi } from "@/hooks/api/useUsersApi";
-import { IUser, IUserPost } from "@/interfaces/IUser";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import { toast } from "sonner";
+import { IUser } from "@/interfaces/IUser";
+import { useParams } from "react-router-dom";
 import { weightTab } from "./UserDashboard";
+import useUserQuery from "@/hooks/queries/user/useUserQuery";
+import useAddUser from "@/hooks/mutations/User/useAddUser";
+import useUpdateUser from "@/hooks/mutations/User/useUpdateUser";
 
 const UserFormPage = () => {
   const { id } = useParams();
-  const navigation = useNavigate();
-  const { getUser, addUser, updateUser } = useUsersApi();
 
-  const [user, setUser] = useState<IUser | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const queryClient = useQueryClient();
-
-  const onSuccess = () => {
-    toast.success(`משתמש נשמר בהצלחה!`);
-    queryClient.invalidateQueries({ queryKey: [`users`] });
-    navigation(MainRoutes.USERS);
-  };
-
-  const onError = (e: any) => {
-    toast.error(ERROR_MESSAGES.GENERIC_ERROR_MESSAGE, { description: e.data.message });
-  };
-
-  const editUser = useMutation({
-    mutationFn: ({ id, user }: { id: string; user: IUser }) => updateUser(id, user),
-    onSuccess: onSuccess,
-    onError: onError,
-  });
-
-  const addNewUser = useMutation({
-    mutationFn: addUser,
-    onSuccess: onSuccess,
-    onError: onError,
-  });
+  const { data: user, isLoading } = useUserQuery(id);
+  const editUser = useUpdateUser();
+  const addNewUser = useAddUser();
 
   const handleSaveUser = (user: IUser) => {
     if (id) {
@@ -50,16 +24,6 @@ const UserFormPage = () => {
       addNewUser.mutate(user);
     }
   };
-
-  useEffect(() => {
-    if (!id) return;
-
-    setIsLoading(true);
-    getUser(id)
-      .then((res) => setUser(res))
-      .catch((err) => console.log(err))
-      .finally(() => setIsLoading(false));
-  }, []);
 
   if (isLoading) return <Loader size="large" />;
 
