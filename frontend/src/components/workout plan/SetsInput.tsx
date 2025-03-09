fb199ac8b8ff56e086c6aa45307d64e1334c0ba4
mike16345 <95699872+mike16345@users.noreply.github.com>
2025-03-09 17:23:43 +0200

    implemented form and errors in sets

--- frontend/src/components/workout plan/SetsInput.tsx
+++ frontend/src/components/workout plan/SetsInput.tsx
@@ -1,42 +1,50 @@
 import React from "react";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
+import { useFormContext } from "react-hook-form";
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
 
 interface SetInputProps {
   setNumber: number;
-  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  maxReps?: number;
-  minReps: number;
+  parentPath: `workoutPlans.${number}.muscleGroups.${number}.exercises.${number}.sets.${number}`;
 }
 
-const SetsInput: React.FC<SetInputProps> = ({ setNumber, handleChange, maxReps, minReps }) => {
+const SetsInput: React.FC<SetInputProps> = ({ setNumber, parentPath }) => {
+  const { control } = useFormContext();
+
   return (
     <div className="flex items-center gap-3">
       <div className="mt-5 font-semibold">סט {setNumber}</div>
-      <div>
-        <Label>מינימום חזרות</Label>
-        <Input
-          name="minReps"
-          type="number"
-          min={0}
-          className="w-28"
-          placeholder="8/10/12..."
-          value={minReps}
-          onChange={(e) => handleChange(e)}
-        />
-      </div>
-      <div>
-        <Label>מקסימום חזרות</Label>
-        <Input
-          name="maxReps"
-          type="number"
-          className="w-28"
-          min={0}
-          placeholder="8/10/12..."
-          value={maxReps}
-          onChange={(e) => handleChange(e)}
-        />
-      </div>
+      <FormField
+        control={control}
+        name={`${parentPath}.minReps`}
+        render={({ field }) => {
+          return (
+            <FormItem>
+              <FormLabel>מינימום חזרות</FormLabel>
+              <FormControl>
+                <Input {...field} type="number" min={0} className="w-28" placeholder="8/10/12..." />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          );
+        }}
+      />
+      <FormField
+        control={control}
+        name={`${parentPath}.maxReps`}
+        render={({ field }) => {
+          return (
+            <FormItem>
+              <FormLabel>מקסימום חזרות</FormLabel>
+              <FormControl>
+                <Input {...field} type="number" min={0} className="w-28" placeholder="8/10/12..." />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          );
+        }}
+      />
     </div>
   );
 };
