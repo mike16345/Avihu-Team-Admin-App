abebf0c635f4f0315880f330076ea7386b84e482
mike16345 <95699872+mike16345@users.noreply.github.com>
2025-03-10 15:53:03 +0200

    used enum key

--- frontend/src/hooks/queries/MuscleGroups/useMuscleGroupsQuery.tsx
+++ frontend/src/hooks/queries/MuscleGroups/useMuscleGroupsQuery.tsx
@@ -1,4 +1,5 @@
 import { FULL_DAY_STALE_TIME } from "@/constants/constants";
+import { QueryKeys } from "@/enums/QueryKeys";
 import useMuscleGroupsApi from "@/hooks/api/useMuscleGroupsApi";
 import { useQuery } from "@tanstack/react-query";
 
@@ -6,7 +7,7 @@ const useMuscleGroupsQuery = () => {
   const { getAllMuscleGroups } = useMuscleGroupsApi();
 
   return useQuery({
-    queryKey: ["muscleGroups"],
+    queryKey: [QueryKeys.MUSCLE_GROUP],
     queryFn: getAllMuscleGroups,
     staleTime: FULL_DAY_STALE_TIME,
   });
