c1d3b5fe8ae1d34af5fe8d144d64bea3768e782b
mike16345 <95699872+mike16345@users.noreply.github.com>
2025-07-29 23:20:35 +0300

    chore: update apiConfig for improved server configuration handling

--- frontend/src/config/apiConfig.ts
+++ frontend/src/config/apiConfig.ts
@@ -4,7 +4,7 @@ const SERVER = import.meta.env.VITE_SERVER;
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: SERVER,
-  timeout: 10000,
+  timeout: 20000,
 });
 
 export default axiosInstance;
