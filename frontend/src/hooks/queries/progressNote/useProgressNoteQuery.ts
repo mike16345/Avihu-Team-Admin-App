291f528d685457faa40eefde269b8bb51308152f
Noam Moskowitz <noammz101@gmail.com>
2025-09-07 14:38:51 +0300

    fixed caching bug

--- frontend/src/hooks/queries/progressNote/useProgressNoteQuery.ts
+++ frontend/src/hooks/queries/progressNote/useProgressNoteQuery.ts
@@ -11,7 +11,7 @@ const useProgressNoteQuery = (userId?: string) => {
 
   return useQuery<any, any, ApiResponse<IProgressNotes>, any>({
     queryFn: () => getProgressNotesByUserId(userId!),
-    queryKey: [QueryKeys.USER_PROGRESS_NOTES, userId],
+    queryKey: [QueryKeys.USER_PROGRESS_NOTES + userId],
     staleTime: FULL_DAY_STALE_TIME / 2,
     retry: createRetryFunction(404, 3),
     enabled: !!userId,
