3c87d9606b46ba6758f86964ff56b61ac97f4995
Noam Moskowitz <noammz101@gmail.com>
2025-09-15 19:31:33 +0300

    added closing of sheet to onSuccess

--- frontend/src/hooks/mutations/progressNotes/useAddProgressNote.tsx
+++ frontend/src/hooks/mutations/progressNotes/useAddProgressNote.tsx
@@ -3,13 +3,18 @@ import { IPostProgressNoteObject } from "@/interfaces/IProgress";
 import { useMutation } from "@tanstack/react-query";
 import { onError, onSuccess } from "@/lib/query";
 import { QueryKeys } from "@/enums/QueryKeys";
+import { useProgressNoteContext } from "@/context/useProgressNoteContext";
 
 const useAddProgressNote = (userId: string) => {
   const { addProgressNote } = useProgressNotesApi();
+  const { setOpenProgressSheet } = useProgressNoteContext();
 
   return useMutation({
     mutationFn: (noteToPost: IPostProgressNoteObject) => addProgressNote(noteToPost),
-    onSuccess: () => onSuccess("פתק נשמר בהצלחה!", [QueryKeys.USER_PROGRESS_NOTES + userId]),
+    onSuccess: () => {
+      onSuccess("פתק נשמר בהצלחה!", [QueryKeys.USER_PROGRESS_NOTES + userId]);
+      setOpenProgressSheet(false);
+    },
     onError: onError,
   });
 };
