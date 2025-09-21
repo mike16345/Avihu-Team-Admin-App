d71f5312649fcd45dd0d147691a52cf9cb5ccb9d
Noam Moskowitz <noammz101@gmail.com>
2025-09-21 09:18:17 +0300

    created measurements interface, hook, and query

---
+++ frontend/src/hooks/queries/measurements/useMeasurementQuery.tsx
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+import { IUserMuscleMeasurements } from "@/interfaces/measurements";
+import { useMeasurementApi } from "@/hooks/api/useMeasurementsApi";
+import { HALF_DAY_STALE_TIME } from "@/constants/constants";
+import { QueryKeys } from "@/enums/QueryKeys";
+
+const useMeasurementQuery = (userId: string) => {
+  const { getMeasurements } = useMeasurementApi();
+
+  return useQuery<any, any, IUserMuscleMeasurements, any>({
+    queryFn: () => getMeasurements(userId!),
+    queryKey: [QueryKeys.USER_MEASUREMENTS + userId],
+    enabled: !!userId,
+    staleTime: HALF_DAY_STALE_TIME,
+  });
+};
+
+export default useMeasurementQuery;
