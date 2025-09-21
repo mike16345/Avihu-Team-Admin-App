b21b746f27b66b61afaeeec808c931e5496c47df
Noam Moskowitz <noammz101@gmail.com>
2025-09-21 10:08:08 +0300

    added retry function to query

--- frontend/src/hooks/queries/measurements/useMeasurementQuery.tsx
+++ frontend/src/hooks/queries/measurements/useMeasurementQuery.tsx
@@ -3,14 +3,16 @@ import { IUserMuscleMeasurements } from "@/interfaces/measurements";
 import { useMeasurementApi } from "@/hooks/api/useMeasurementsApi";
 import { HALF_DAY_STALE_TIME } from "@/constants/constants";
 import { QueryKeys } from "@/enums/QueryKeys";
+import { createRetryFunction } from "@/lib/utils";
 
-const useMeasurementQuery = (userId: string) => {
+const useMeasurementQuery = (userId?: string) => {
   const { getMeasurements } = useMeasurementApi();
 
   return useQuery<any, any, IUserMuscleMeasurements, any>({
     queryFn: () => getMeasurements(userId!),
     queryKey: [QueryKeys.USER_MEASUREMENTS + userId],
     enabled: !!userId,
+    retry: createRetryFunction(404, 2),
     staleTime: HALF_DAY_STALE_TIME,
   });
 };
