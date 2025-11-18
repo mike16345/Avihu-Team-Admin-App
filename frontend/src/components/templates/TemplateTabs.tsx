7a92fbc9f9ba5173c6163c7455bbac6a3571aca9
mike16345 <95699872+mike16345@users.noreply.github.com>
2025-11-18 17:09:14 +0200

    removed log

--- frontend/src/components/templates/TemplateTabs.tsx
+++ frontend/src/components/templates/TemplateTabs.tsx
@@ -116,8 +116,6 @@ const TemplateTabs: React.FC<TemplateTabsProps> = ({ tabs }) => {
     setIsSheetOpen(true);
   }, [selectedObjectId]);
 
-  console.log("apiData", apiData?.error?.status);
-
   if (apiData.isError && apiData?.error?.status !== 404)
     return <ErrorPage message={apiData.error.message} />;
 
