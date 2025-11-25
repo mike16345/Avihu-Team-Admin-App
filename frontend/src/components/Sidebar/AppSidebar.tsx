f50f645cb8086f980fa1a30c99a1ad1bee4fa623
Noam Moskowitz <noammz101@gmail.com>
2025-11-25 11:10:28 +0200

    CHANGE: Changed the items to a double array and moved leads into its own category

--- frontend/src/components/Sidebar/AppSidebar.tsx
+++ frontend/src/components/Sidebar/AppSidebar.tsx
@@ -29,6 +29,7 @@ import LogoutButton from "../Navbar/LogoutButton";
 import { ModeToggle } from "../theme/mode-toggle";
 import { useUsersStore } from "@/store/userStore";
 import { userFullName } from "@/lib/utils";
+import { Separator } from "../ui/separator";
 
 type LinkProps = {
   title: string;
@@ -43,43 +44,41 @@ type SidebarItem = {
   children?: LinkProps[];
 };
 
-const items: SidebarItem[] = [
-  {
-    url: "/",
-    title: "בית",
-    icon: Home,
-  },
-  {
-    title: "לקוחות",
-    icon: User,
-    children: [
-      {
-        url: "/users",
-        title: "משתמשים",
-        icon: User,
-      },
-      {
-        url: "/leads",
-        title: "לידים",
-        icon: Inbox,
-      },
-    ],
-  },
-  {
-    url: "/blogs",
-    title: "מאמרים",
-    icon: Edit,
-  },
-  {
-    url: "/dietPlans",
-    title: "תפריטים",
-    icon: SquareMenu,
-  },
-  {
-    url: "/workoutPlans",
-    title: "תוכנית אימון",
-    icon: BicepsFlexed,
-  },
+const sidebarGroups: SidebarItem[][] = [
+  [
+    {
+      url: "/",
+      title: "בית",
+      icon: Home,
+    },
+    {
+      url: "/users",
+      title: "משתמשים",
+      icon: User,
+    },
+    {
+      url: "/blogs",
+      title: "מאמרים",
+      icon: Edit,
+    },
+    {
+      url: "/dietPlans",
+      title: "תפריטים",
+      icon: SquareMenu,
+    },
+    {
+      url: "/workoutPlans",
+      title: "תוכנית אימון",
+      icon: BicepsFlexed,
+    },
+  ],
+  [
+    {
+      url: "/leads",
+      title: "לידים",
+      icon: Inbox,
+    },
+  ],
 ];
 
 const SidebarItems = () => {
@@ -87,70 +86,75 @@ const SidebarItems = () => {
 
   return (
     <>
-      {items.map((item) => {
-        const hasChildren = Boolean(item.children?.length);
-        const isActive = hasChildren
-          ? item.children?.some((child) => child.url === location.pathname)
-          : location.pathname === item.url;
+      {sidebarGroups.map((group, index) => (
+        <>
+          {!!index && <Separator className="my-4" />}
+          {group.map((item) => {
+            const hasChildren = Boolean(item.children?.length);
+            const isActive = hasChildren
+              ? item.children?.some((child) => child.url === location.pathname)
+              : location.pathname === item.url;
 
-        if (!hasChildren && item.url) {
-          return (
-            <SidebarMenuItem key={item.title}>
-              <SidebarMenuButton asChild isActive={isActive}>
-                <Link
-                  className={`w-full rounded-full ${
-                    isActive && "text-secondary bg-secondary-foreground"
-                  }`}
-                  to={item.url}
-                >
-                  <item.icon />
-                  <span>{item.title}</span>
-                </Link>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
-          );
-        }
+            if (!hasChildren && item.url) {
+              return (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild isActive={isActive}>
+                    <Link
+                      className={`w-full rounded-full ${
+                        isActive && "text-secondary bg-secondary-foreground"
+                      }`}
+                      to={item.url}
+                    >
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              );
+            }
 
-        return (
-          <SidebarMenuItem key={item.title}>
-            <Collapsible defaultOpen={Boolean(isActive)} className="group/collapsible">
-              <CollapsibleTrigger className="flex items-center w-full justify-between" asChild>
-                <SidebarMenuButton isActive={Boolean(isActive)}>
-                  <div className="flex items-center gap-2">
-                    <item.icon />
-                    <span>{item.title}</span>
-                  </div>
-                  <ChevronDown className="mr-auto h-4 w-4 transition-transform group-data-[state=open]/collapsible:rotate-180" />
-                </SidebarMenuButton>
-              </CollapsibleTrigger>
+            return (
+              <SidebarMenuItem key={item.title}>
+                <Collapsible defaultOpen={Boolean(isActive)} className="group/collapsible">
+                  <CollapsibleTrigger className="flex items-center w-full justify-between" asChild>
+                    <SidebarMenuButton isActive={Boolean(isActive)}>
+                      <div className="flex items-center gap-2">
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </div>
+                      <ChevronDown className="mr-auto h-4 w-4 transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                    </SidebarMenuButton>
+                  </CollapsibleTrigger>
 
-              <CollapsibleContent>
-                <SidebarMenuSub>
-                  {item.children?.map((child) => {
-                    const childActive = location.pathname === child.url;
+                  <CollapsibleContent>
+                    <SidebarMenuSub>
+                      {item.children?.map((child) => {
+                        const childActive = location.pathname === child.url;
 
-                    return (
-                      <SidebarMenuSubItem key={child.title}>
-                        <SidebarMenuSubButton asChild isActive={childActive}>
-                          <Link
-                            className={`flex items-center gap-2 ${
-                              childActive && "text-secondary bg-secondary"
-                            }`}
-                            to={child.url}
-                          >
-                            {child.icon ? <child.icon /> : null}
-                            <span>{child.title}</span>
-                          </Link>
-                        </SidebarMenuSubButton>
-                      </SidebarMenuSubItem>
-                    );
-                  })}
-                </SidebarMenuSub>
-              </CollapsibleContent>
-            </Collapsible>
-          </SidebarMenuItem>
-        );
-      })}
+                        return (
+                          <SidebarMenuSubItem key={child.title}>
+                            <SidebarMenuSubButton asChild isActive={childActive}>
+                              <Link
+                                className={`flex items-center gap-2 ${
+                                  childActive && "text-secondary bg-secondary"
+                                }`}
+                                to={child.url}
+                              >
+                                {child.icon ? <child.icon /> : null}
+                                <span>{child.title}</span>
+                              </Link>
+                            </SidebarMenuSubButton>
+                          </SidebarMenuSubItem>
+                        );
+                      })}
+                    </SidebarMenuSub>
+                  </CollapsibleContent>
+                </Collapsible>
+              </SidebarMenuItem>
+            );
+          })}
+        </>
+      ))}
     </>
   );
 };
