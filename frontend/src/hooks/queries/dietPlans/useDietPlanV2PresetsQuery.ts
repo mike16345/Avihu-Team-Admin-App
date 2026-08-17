0b5c6636a7fc73d1782ef8723ab11d99c1241c37
mike16345 <95699872+mike16345@users.noreply.github.com>
2026-08-17 16:58:13 -0400

    feat: include trainerId in query keys for blogs, cardio workouts, diet plans, exercises, and form responses

--- frontend/src/hooks/queries/dietPlans/useDietPlanV2PresetsQuery.ts
+++ frontend/src/hooks/queries/dietPlans/useDietPlanV2PresetsQuery.ts
@@ -1,13 +1,16 @@
 import { FULL_DAY_STALE_TIME } from "@/constants/constants";
 import { QueryKeys } from "@/enums/QueryKeys";
 import { useDietPlanPresetApi } from "@/hooks/api/useDietPlanPresetsApi";
+import { useUsersStore } from "@/store/userStore";
 import { useQuery } from "@tanstack/react-query";
 
 const useDietPlanV2PresetsQuery = (enabled = true) => {
+  const trainerId = useUsersStore((state) => state.currentUser?.trainerId) || "";
+
   const { getAllDietPlanV2Presets } = useDietPlanPresetApi();
 
   return useQuery({
-    queryKey: [QueryKeys.DIET_PLAN_PRESETS, 2],
+    queryKey: [QueryKeys.DIET_PLAN_PRESETS, 2, trainerId],
     queryFn: getAllDietPlanV2Presets,
     staleTime: FULL_DAY_STALE_TIME / 2,
     enabled,
